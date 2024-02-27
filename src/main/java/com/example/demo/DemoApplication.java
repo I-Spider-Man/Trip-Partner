afa377acac2102606fc108f276dd366ea8c15461
I-Spider-Man <153480960+I-Spider-Man@users.noreply.github.com>
2024-02-28 00:05:58 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -21,7 +21,7 @@ public class DemoApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://13.127.117.206:3001","http://13.127.117.206:3000")
+						.allowedOrigins("http://13.233.162.0:3001","http://13.201.35.23:3000")
 						.allowedMethods("GET", "POST", "PUT", "DELETE")
 						.allowedHeaders("*");
 			}
